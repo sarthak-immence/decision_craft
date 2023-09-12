228f2074e1698566ce6922bbaff06ae7e872f57c
Sarthak <sarthak.s@immence.in>
2023-09-12 10:31:17 +0530

    first commit

---
+++ src/redux/rootReducer.ts
@@ -0,0 +1,46 @@
+/* eslint-disable import/no-named-as-default */
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
+// import storage from 'redux-persist/lib/storage';
+// slices
+
+// import masterSlices from './master/slices/masterSlices';
+
+// ----------------------------------------------------------------------
+
+export const createNoopStorage = () => ({
+  getItem(_key: string) {
+    return Promise.resolve(null);
+  },
+  setItem(_key: string, value: any) {
+    return Promise.resolve(value);
+  },
+  removeItem(_key: string) {
+    return Promise.resolve();
+  },
+});
+
+export const storage =
+  typeof window !== 'undefined' ? createWebStorage('local') : createNoopStorage();
+
+export const rootPersistConfig = {
+  key: 'root',
+  storage,
+  keyPrefix: 'redux-',
+  whitelist: [],
+};
+
+export const productPersistConfig = {
+  key: 'product',
+  storage,
+  keyPrefix: 'redux-',
+  whitelist: ['sortBy', 'checkout'],
+};
+
+const rootReducer = combineReducers({
+  master: masterSlices,
+  
+});
+
+export default rootReducer;
